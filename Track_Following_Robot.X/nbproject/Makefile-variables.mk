71d5c16e567b38c7c221c9c20c1b3f30b8def7fe
Anmol Gill <anmol_gill09@live.com>
2020-03-29 16:07:53 -0700

    Added sample program

---
+++ Track_Following_Robot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Track_Following_Robot.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Track_Following_Robot.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=trackfollowingrobot.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/trackfollowingrobot.x.tar
